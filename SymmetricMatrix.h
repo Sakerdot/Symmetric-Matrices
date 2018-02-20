fc096f601d0509099c53cb44b1c9273f941f49f6
Adrian <adriansamatan@gmail.com>
2018-02-20 13:43:38 +0100

    more operators added and modified other ones

--- SymmetricMatrix.h
+++ SymmetricMatrix.h
@@ -44,11 +44,9 @@ public:
     initArray();
   }
 
-  template <typename Scalar, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
-  SymMat(const Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &other)
+  template <int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+  SymMat(const Eigen::Matrix<_Scalar, Rows, Cols, Options, MaxRows, MaxCols> &other)
   {
-    static_assert(std::is_same<_Scalar, Scalar>::value, "SymMat and Eigen::Matrix don't have the same Scalar type.");
-
     m_order = other.rows();
 
     if (m_order == other.cols())
@@ -73,7 +71,10 @@ public:
 
   ~SymMat()
   {
-    delete[] m_data;
+    if (m_data)
+    {
+      delete[] m_data;
+    }
   }
 
   inline SymMat operator+(const SymMat &other) const
@@ -82,11 +83,31 @@ public:
     {
       SymMat addition(m_order);
 
+      for (int i = arraySize() - 1; i >= 0; --i)
+      {
+        addition.m_data[i] = m_data[i] + other.m_data[i];
+      }
+
+      return addition;
+    }
+    else
+    {
+      // throw exception
+    }
+  }
+
+  template <int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+  inline auto operator+(const Eigen::Matrix<_Scalar, Rows, Cols, Options, MaxRows, MaxCols> &other) const
+  {
+    if (other.rows() == m_order && other.cols() == m_order && m_data)
+    {
+      Eigen::Matrix<_Scalar, Rows, Cols, Options, MaxRows, MaxCols> addition(other.rows(), other.cols());
+
       for (int i = 0; i < m_order; ++i)
       {
-        for (int j = i; j < m_order; ++j)
+        for (int j = 0; j < m_order; ++j)
         {
-          addition(i, j) = other(i, j) + (*this)(i, j);
+          addition(i, j) = (*this)(i, j) + other(i, j);
         }
       }
 
@@ -98,7 +119,78 @@ public:
     }
   }
 
-  inline _Scalar& operator()(Eigen::Index row, Eigen::Index col)
+  inline SymMat operator-(const SymMat &other) const
+  {
+    if (m_order == other.m_order && m_data && other.m_data)
+    {
+      SymMat subtraction(m_order);
+
+      for (int i = arraySize() - 1; i >= 0; --i)
+      {
+        subtraction.m_data[i] = m_data[i] - other.m_data[i];
+      }
+
+      return subtraction;
+    }
+    else
+    {
+      // throw exception
+    }
+  }
+
+  template <int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+  inline auto operator-(const Eigen::Matrix<_Scalar, Rows, Cols, Options, MaxRows, MaxCols> &other) const
+  {
+    if (other.rows() == m_order && other.cols() == m_order && m_data)
+    {
+      Eigen::Matrix<_Scalar, Rows, Cols, Options, MaxRows, MaxCols> subtraction(other.rows(), other.cols());
+
+      for (int i = 0; i < m_order; ++i)
+      {
+        for (int j = 0; j < m_order; ++j)
+        {
+          subtraction(i, j) = (*this)(i, j) - other(i, j);
+        }
+      }
+
+      return subtraction;
+    }
+    else
+    {
+      // throw exception
+    }
+  }
+
+  inline SymMat operator*(const SymMat &other) const
+  {
+    if (m_order == other.m_order && m_data && other.m_data)
+    {
+      Eigen::Matrix<_Scalar, Eigen::Dynamic, Eigen::Dynamic> product(m_order, m_order);
+
+      for (int i = 0; i < m_order; ++i)
+      {
+        for (int j = 0; j < m_order; ++j)
+        {
+          _Scalar sum = 0;
+
+          for (int k = 0; k < m_order; ++k)
+          {
+            sum += (*this)(i, k) * other(k, j);
+          }
+
+          product(i, j) = sum;
+        }
+      }
+
+      return product;
+    }
+    else
+    {
+      // throw exception
+    }
+  }
+
+  inline _Scalar &operator()(Eigen::Index row, Eigen::Index col)
   {
     if (row >= 0 && row < m_order && col >= 0 && col < m_order && m_data)
     {
@@ -115,7 +207,7 @@ public:
     }
   }
 
-  inline _Scalar& operator()(Eigen::Index row, Eigen::Index col) const
+  inline _Scalar &operator()(Eigen::Index row, Eigen::Index col) const
   {
     if (row >= 0 && row < m_order && col >= 0 && col < m_order && m_data)
     {
@@ -135,18 +227,20 @@ public:
 private:
   void initArray()
   {
-    m_data = new _Scalar[m_order * (m_order + 1) / 2];
+    m_data = new _Scalar[arraySize()];
   }
 
   void copyArray(const SymMat &other)
   {
-    for (int i = 0; i < m_order; ++i)
+    for (int i = arraySize() - 1; i >= 0; --i)
     {
-      for (int j = i; j < m_order; ++j)
-      {
-        (*this)(i, j) = other(i, j);
-      }
+      m_data[i] = other.m_data[i];
     }
+  }
+
+  int arraySize() const
+  {
+    return m_order * (m_order + 1) / 2;
   }
 
 private:
